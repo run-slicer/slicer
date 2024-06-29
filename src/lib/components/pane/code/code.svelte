7b4c689e718b440742d1468b3c42a8c8a792aacb
Matouš Kučera <mk@kcra.me>
2024-06-29 12:09:33 +0200

    fix: update CFR, remove empty loading

--- src/lib/components/pane/code/code.svelte
+++ src/lib/components/pane/code/code.svelte
@@ -15,39 +15,43 @@
 <div class="flex h-full w-full flex-col scrollbar-thin">
     <PaneHeader name="Code" icon={Code} />
     <div class="relative basis-full overflow-hidden">
-        {#await Promise.all([import("svelte-codemirror-editor"), load(language), read(entry)])}
-            <Loading value={entry?.type === "class" ? "Decompiling..." : "Reading..."} overlay />
-        {:then [editor, lang, value]}
-            <svelte:component
-                this={editor.default}
-                {value}
-                readonly
-                theme={$mode === "dark" ? dark : light}
-                {lang}
-                class="absolute h-full w-full"
-                styles={{
-                    "&": {
-                        // make editor 100% height of parent
-                        height: "100%",
-                    },
-                    ".cm-line": {
-                        // add some scrolling room to line ends
-                        "padding-right": "2.5rem",
-                    },
-                    ".cm-foldPlaceholder": {
-                        // fix color on collapse placeholder
-                        "background-color": "hsl(var(--muted))",
-                        border: "unset",
-                        color: "unset",
-                        margin: "0 0.25rem",
-                        padding: "0 0.2rem",
-                    },
-                    ".cm-scroller": {
-                        // make scrollbar thinner
-                        "scrollbar-width": "thin",
-                    },
-                }}
-            />
-        {/await}
+        {#if entry}
+            {#await Promise.all([import("svelte-codemirror-editor"), load(language), read(entry)])}
+                <Loading value={entry.type === "class" ? "Decompiling..." : "Reading..."} overlay />
+            {:then [editor, lang, value]}
+                <svelte:component
+                    this={editor.default}
+                    {value}
+                    readonly
+                    theme={$mode === "dark" ? dark : light}
+                    {lang}
+                    class="absolute h-full w-full"
+                    styles={{
+                        "&": {
+                            // make editor 100% height of parent
+                            height: "100%",
+                        },
+                        ".cm-line": {
+                            // add some scrolling room to line ends
+                            "padding-right": "2.5rem",
+                        },
+                        ".cm-foldPlaceholder": {
+                            // fix color on collapse placeholder
+                            "background-color": "hsl(var(--muted))",
+                            border: "unset",
+                            color: "unset",
+                            margin: "0 0.25rem",
+                            padding: "0 0.2rem",
+                        },
+                        ".cm-scroller": {
+                            // make scrollbar thinner
+                            "scrollbar-width": "thin",
+                        },
+                    }}
+                />
+            {/await}
+        {:else}
+            <!-- fancy welcome goes here -->
+        {/if}
     </div>
 </div>
