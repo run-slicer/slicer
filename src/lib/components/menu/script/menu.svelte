7395bd3e3674577b003fd5d9caaf769db74a677f
Matouš Kučera <mk@kcra.me>
2024-08-17 17:47:09 +0200

    [ci skip] style: prettier

--- src/lib/components/menu/script/menu.svelte
+++ src/lib/components/menu/script/menu.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-    import { MenubarSeparator, MenubarSub, MenubarSubContent, MenubarSubTrigger } from "$lib/components/ui/menubar";
+    import {
+        MenubarCheckboxItem,
+        MenubarSeparator,
+        MenubarSub,
+        MenubarSubContent,
+        MenubarSubTrigger,
+    } from "$lib/components/ui/menubar";
     import { load, unload, ScriptState, type ProtoScript } from "$lib/script";
     import ScriptOption from "./option.svelte";
-    import { MenubarCheckboxItem } from "$lib/components/ui/menubar/index.js";
 
     export let proto: ProtoScript;
     const script = proto.script;
