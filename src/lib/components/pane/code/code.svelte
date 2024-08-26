d7ce3d0c6f158f6305e4315937a42d1973ba3ae9
Matouš Kučera <mk@kcra.me>
2024-08-26 15:01:53 +0200

    feat: text size sync state

--- src/lib/components/pane/code/code.svelte
+++ src/lib/components/pane/code/code.svelte
@@ -9,7 +9,8 @@
     import { Select, SelectContent, SelectItem, SelectTrigger } from "$lib/components/ui/select";
     import { all as disasms } from "$lib/disasm";
     import vf from "$lib/disasm/vf";
-    import { toolsDisasm, editorTextSize } from "$lib/state";
+    import { toolsDisasm, editorTextSize, editorTextSizeSync } from "$lib/state";
+    import { get, writable } from "svelte/store";
 
     export let tab: Tab;
     let entry = tab.entry!;
@@ -24,13 +25,17 @@
     $: disasm = disasms.get(disasmProto.value) || vf;
 
     $: language = detectLanguage(tab.type, entry, disasm);
+
+    $: textSize = $editorTextSizeSync
+        ? editorTextSize
+        : writable<number>(get(editorTextSize) /* immediate value, no subscription */);
 </script>
 
 <div class="relative basis-full overflow-hidden scrollbar-thin">
     {#await Promise.all([import("./editor.svelte"), loadLanguage(language), read(tab.type, entry, disasm)])}
         <Loading value={shouldDisasm ? "Disassembling..." : "Reading..."} overlay />
     {:then [editor, lang, value]}
-        <svelte:component this={editor.default} {value} readOnly {lang} bind:textSize={$editorTextSize} />
+        <svelte:component this={editor.default} {value} readOnly {lang} bind:textSize={$textSize} />
     {/await}
     {#if shouldDisasm}
         <div class="absolute bottom-0 right-0 m-[15px]">
--- src/lib/state.ts
+++ src/lib/state.ts
@@ -14,3 +14,4 @@ export const loggingOpen = persisted<boolean>(`${root}.logging.open`, false);
 export const loggingMaxEntries = persisted<number>(`${root}.logging.max-entries`, 50);
 export const scriptingScripts = persisted<ScriptData[]>(`${root}.scripting.scripts`, []);
 export const editorTextSize = persisted<number>(`${root}.editor.text-size`, 12);
+export const editorTextSizeSync = persisted<boolean>(`${root}.editor.text-size.sync`, true);
