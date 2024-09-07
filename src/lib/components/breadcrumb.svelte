5f437a06fe3d9e00135a2076b8746d3386ad59e3
Matouš Kučera <mk@kcra.me>
2024-09-07 17:30:02 +0200

    refactor: visual changes

--- src/lib/components/breadcrumb.svelte
+++ src/lib/components/breadcrumb.svelte
@@ -9,16 +9,16 @@
 </script>
 
 <Separator />
-<div class="flex h-6 items-center overflow-x-auto px-2 text-sm text-muted-foreground scrollbar-none">
+<div class="flex h-6 items-center overflow-x-auto px-2 scrollbar-none">
     {#if path}
         {@const parts = path.split("/")}
         <Breadcrumb>
-            <BreadcrumbList class="flex-nowrap">
+            <BreadcrumbList class="flex-nowrap text-xs">
                 {#each parts as part, i}
                     {@const last = i === parts.length - 1}
                     <BreadcrumbItem>
                         {#if last && icon}
-                            <svelte:component this={icon.icon} size={16} class={cn("min-w-[16px]", icon.classes)} />
+                            <svelte:component this={icon.icon} size={14} class={cn("min-w-[14px]", icon.classes)} />
                         {/if}
                         {part}
                     </BreadcrumbItem>
