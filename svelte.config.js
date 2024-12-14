cdd0b1ad49b963e7ef2598844d26157f00807cd7
Matouš Kučera <mk@kcra.me>
2024-12-14 15:33:29 +0100

    chore: ignore compiler warnings

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,14 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+const ignoredWarnings = [
+    "state_referenced_locally", // we're using (immediate) $state values in $state defaults, ignore
+];
+
 export default {
     // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
     // for more information about preprocessors
     preprocess: vitePreprocess(),
+    compilerOptions: {
+        warningFilter: (w) => !ignoredWarnings.includes(w.code),
+    },
 };
