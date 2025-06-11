8ce4cd2e3a5d6c7f7211c3c18f031984bfddf3ec
Matouš Kučera <mk@kcra.me>
2025-06-11 18:19:19 +0200

    fix: Safari

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,9 @@ import { mount } from "svelte";
 import App from "./app.svelte";
 import "./main.css";
 
+import { polyfill } from "wasm-imports-parser/polyfill";
+
+// this needs to be polyfilled because Safari has trouble parsing TeaVM output
+globalThis.WebAssembly = polyfill(globalThis.WebAssembly);
+
 export default mount(App, { target: document.getElementById("app")! });
