cce3d333a324f3bdb883f8d7d9354554aeec6064
Matouš Kučera <mk@kcra.me>
2025-09-28 19:58:16 +0200

    refactor: bump ASM

--- src/lib/components/pane/class/overview.svelte
+++ src/lib/components/pane/class/overview.svelte
@@ -1,10 +1,23 @@
+<script lang="ts" module>
+    export const specialVer: Record<number, string> = {
+        45: "1.0.2 or 1.1",
+        46: "1.2",
+        47: "1.3",
+        48: "1.4",
+    };
+</script>
+
 <script lang="ts">
     import type { Node } from "@katana-project/asm";
     import type { UTF8Entry } from "@katana-project/asm/pool";
     import { Table, TableCell, TableHead, TableRow } from "$lib/components/ui/table";
     import { ElementType, formatMod } from "@katana-project/asm/analysis/disasm";
-    import { AttributeType, Modifier, Version } from "@katana-project/asm/spec";
-    import type { SignatureAttribute, SourceFileAttribute } from "@katana-project/asm/attr";
+    import { AttributeType, Modifier } from "@katana-project/asm/spec";
+    import type {
+        PermittedSubclassesAttribute,
+        SignatureAttribute,
+        SourceFileAttribute,
+    } from "@katana-project/asm/attr";
 
     interface Props {
         node: Node;
@@ -12,10 +25,10 @@
 
     let { node }: Props = $props();
 
+    const permittedSubclassesAttr = node.attrs.find((a) => a.type === AttributeType.PERMITTED_SUBCLASSES);
     const signatureAttr = node.attrs.find((a) => a.type === AttributeType.SIGNATURE);
     const sourceFileAttr = node.attrs.find((a) => a.type === AttributeType.SOURCE_FILE);
 
-    const version = Version[node.major]?.substring(2)?.replaceAll("_", ".") || `${node.major - 44}?`;
     const name = (node.pool[node.thisClass.name] as UTF8Entry).string;
     const mods = formatMod(
         node.access,
@@ -23,6 +36,11 @@
     );
     const superName = node.superClass ? (node.pool[node.superClass.name] as UTF8Entry).string : null;
     const interfaces = node.interfaces.map(({ name }) => (node.pool[name] as UTF8Entry).string);
+
+    const permittedSubclasses =
+        (permittedSubclassesAttr as PermittedSubclassesAttribute)?.classes?.map(
+            ({ entry }) => (node.pool[entry?.name ?? 0] as UTF8Entry).string
+        ) || [];
     const signature = (signatureAttr as SignatureAttribute)?.signatureEntry?.string;
     const sourceFile = (sourceFileAttr as SourceFileAttribute)?.sourceFileEntry?.string;
 </script>
@@ -39,7 +57,14 @@
     </TableRow>
     <TableRow>
         <TableHead>Minor version</TableHead>
-        <TableCell class="font-mono tracking-tight">{node.minor}</TableCell>
+        <TableCell>
+            <span class="font-mono tracking-tight">
+                {node.minor}
+            </span>
+            {#if node.major >= 55}
+                (preview features {node.minor === 65535 ? "enabled" : node.minor === 0 ? "disabled" : "unknown"})
+            {/if}
+        </TableCell>
     </TableRow>
     <TableRow>
         <TableHead>Major version</TableHead>
@@ -47,7 +72,7 @@
             <span class="font-mono tracking-tight">
                 {node.major}
             </span>
-            ({version})
+            ({specialVer[node.major] || (node.major - 44).toString()})
         </TableCell>
     </TableRow>
     <TableRow>
@@ -80,13 +105,27 @@
             {/if}
         </TableCell>
     </TableRow>
-    <TableRow>
-        <TableHead>Signature</TableHead>
-        <TableCell class="break-anywhere font-mono tracking-tight whitespace-normal">{signature || "<none>"}</TableCell>
-    </TableRow>
-    <TableRow>
-        <TableHead>Source file</TableHead>
-        <TableCell class="break-anywhere font-mono tracking-tight whitespace-normal">{sourceFile || "<none>"}</TableCell
-        >
-    </TableRow>
+    {#if permittedSubclasses.length > 0}
+        <TableRow>
+            <TableHead>Permitted subclasses</TableHead>
+            <TableCell class="break-anywhere font-mono tracking-tight whitespace-normal">
+                {#each permittedSubclasses as subclass, i}
+                    {subclass}
+                    {#if i !== permittedSubclasses.length - 1}<br />{/if}
+                {/each}
+            </TableCell>
+        </TableRow>
+    {/if}
+    {#if signature}
+        <TableRow>
+            <TableHead>Signature</TableHead>
+            <TableCell class="break-anywhere font-mono tracking-tight whitespace-normal">{signature}</TableCell>
+        </TableRow>
+    {/if}
+    {#if sourceFile}
+        <TableRow>
+            <TableHead>Source file</TableHead>
+            <TableCell class="break-anywhere font-mono tracking-tight whitespace-normal">{sourceFile}</TableCell>
+        </TableRow>
+    {/if}
 </Table>
